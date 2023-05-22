58104a9329369e52210c7888f032ce0f9e653d7e
Mambet Aibar <109662937+Aibar77@users.noreply.github.com>
2023-05-22 20:40:06 +0600

    finish html block and add svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,94 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div
+        style={{
+          display: "none",
+        }}
+        className="overlay"
+      >
+        <div className="drawer">
+          <h2 className="mb-30 d-flex justify-between">
+            Корзина{" "}
+            <img
+              className="removeBtn"
+              src="/img/Btn-Remove.svg"
+              alt="removebtn"
+            />
+          </h2>
+
+          <div className="items">
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{
+                  backgroundImage: "url(/img/sneakers/1.jpg)",
+                }}
+                className="cartItemImg"
+              ></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/Btn-Remove.svg"
+                alt="removebtn"
+              />
+            </div>
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{
+                  backgroundImage: "url(/img/sneakers/2.jpg)",
+                }}
+                className="cartItemImg"
+              ></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 200</p>
+                <b>8 999 руб.</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/Btn-Remove.svg"
+                alt="removebtn"
+              />
+            </div>
+            <div className="cartItem d-flex align-center mb-20">
+              <div
+                style={{
+                  backgroundImage: "url(/img/sneakers/3.jpg)",
+                }}
+                className="cartItemImg"
+              ></div>
+              <div className="mr-20 flex">
+                <p className="mb-5">Мужские Кроссовки Nike Air Max 300</p>
+                <b>9 999 руб.</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/Btn-Remove.svg"
+                alt="removebtn"
+              />
+            </div>
+          </div>
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Итого: </span>
+                <div></div>
+                <b>21 498 руб. </b>
+              </li>
+              <li>
+                <span>Налог 5%: </span>
+                <div></div>
+                <b>1074 руб. </b>
+              </li>
+            </ul>
+            <button className="greenButton">
+              Оформить заказ <img src="/img/Arrow.svg" alt="Arrow" />
+            </button>
+          </div>
+        </div>
+      </div>
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img width={40} height={40} src="/img/logo.png" alt="logo" />
@@ -21,10 +109,19 @@ function App() {
         </ul>
       </header>
       <div className="content p-40">
-        <h1 className="mb-40">Все кроссовки</h1>
+        <div className="d-flex justify-between align-center mb-40">
+          <h1>Все кроссовки</h1>
+          <div className="search-block">
+            <img src="/img/Search.svg" alt="search-bar" />
+            <input className="search" type="search" placeholder="Поиск..." />
+          </div>
+        </div>
 
-        <div class="sneakers">
+        <div className="sneakers">
           <div className="card">
+            <div className="favourite">
+              <img src="/img/Heart-Unlike.svg" alt="heart-unlike" />
+            </div>
             <img
               width={133}
               height={112}
